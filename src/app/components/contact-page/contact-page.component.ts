94a633f439df8907f92059b28b7124f8dfe7c63e
Nicholas-Baldassini <46302707+Nicholas-Baldassini@users.noreply.github.com>
2021-12-27 21:06:37 -0500

    Finally email is finished

--- src/app/components/contact-page/contact-page.component.ts
+++ src/app/components/contact-page/contact-page.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-contact-page',
   templateUrl: './contact-page.component.html',
-  styleUrls: ['./contact-page.component.css']
+  styleUrls: ['./contact-page.component.css'],
 })
+
 export class ContactPageComponent implements OnInit {
+  sendEmail() {
+    const email = <HTMLInputElement>document.getElementById('emailInput');
+    const phoneNumber = <HTMLInputElement>(
+      document.getElementById('phonenumber')
+    );
+    const name = <HTMLInputElement>document.getElementById('name');
+    const information = <HTMLInputElement>(
+      document.getElementById('informationArea')
+    );
 
-  constructor() { }
+    const localBackend: string = 'http://localhost:3000/sendMail';
+    const liveBackend: string = "https://baldassini-mechanical-backend.herokuapp.com/sendMail";
 
-  ngOnInit(): void {
+    // CHANGE TO 1 WHEN LIVE
+    const URL: string = [localBackend, liveBackend][1];
+
+    const data = {
+      email: email.value,
+      name: name.value,
+      number: phoneNumber.value,
+      text: information.value,
+    };
+
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    fetch(URL, options)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.error('Fetch Error:', error);
+      });
   }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
